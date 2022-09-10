0515d94334f703891d5ec69dc5eacdfca278d9cd
Pierre Depretz <pierre.depretz@mongodb.com>
2022-09-10 19:27:46 +1000

    update

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
 # deletedocs
+
+## New test
+```
+mongo localhost/admin --eval 'db.shutdownServer();'; rm -rf /Users/pdepretz/0_m/standalone/bulkinsert/* && mlaunch init --single
+```
+
+## Bulk insert stats
+```
+mongo localhost/mydatabase /Users/pdepretz/4_apps/bulkinsert.js
+mongo localhost/mydatabase --eval 'db.mycollection.stats( { freeStorage: 1, scale: 1024 } )'
+```
+
+## Delete stats
+```
+mongo localhost/mydatabase --eval 'db.mycollection.deleteMany({ "user_id":{$lt:100000}})'
+```
+
+## WiredTiger
+```
+db.mycollection.stats().wiredTiger
+mongo localhost/mydatabase db.mycollection.stats().block-manager
+```
+
+## Compact
+```
+localhost/mydatabase --eval 'db.runCommand({compact: "mycollection",force:true})'
+mongo localhost/mydatabase --eval "db.mycollection.deleteMany({})"
+```
--- bulkinsert.js
+++ bulkinsert.js
@@ -1,13 +1,3 @@
-// mongo localhost/admin --eval 'db.shutdownServer();'; rm -rf /Users/pdepretz/0_m/standalone/bulkinsert/* && mlaunch init --single
-//1. mongo localhost/mydatabase /Users/pdepretz/4_apps/bulkinsert.js
-//3. mongo localhost/mydatabase --eval 'db.mycollection.stats( { freeStorage: 1, scale: 1024 } )'
-//2. mongo localhost/mydatabase --eval 'db.mycollection.deleteMany({ "user_id":{$lt:100000}})'
-//3. mongo localhost/mydatabase --eval 'db.mycollection.stats( { freeStorage: 1, scale: 1024 } )'
-//3. mongo localhost/mydatabase db.mycollection.stats().block-manager
-//db.mycollection.stats().wiredTiger
-//3. Compact mongo localhost/mydatabase --eval 'db.runCommand({compact: "mycollection",force:true})'
-//2. Delete mongo localhost/mydatabase --eval "db.mycollection.deleteMany({})"
-
 function SampleData(myBulk){
 	people = ["Marc", "Bill", "George", "Eliot", "Matt", "Trey", "Tracy", "Greg", "Steve", "Kristina", "Katie", "Jeff"];
  
